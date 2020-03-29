fdf1d38d45385f947e12079524f7b28128bf209a
Neil Mudjer <neil.mudjer@gmail.com>
2020-03-29 14:52:50 -0500

    preparing for github pages

---
+++ static/js/logic3.js
@@ -0,0 +1,39 @@
+// Store our API endpoint inside queryUrl
+// Store our API endpoint inside queryUrl
+console.log('geo json import!!!!', geoJson);
+
+  // Define streetmap and darkmap layers
+    var light = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "mapbox.light",
+    accessToken: API_KEY
+    });
+
+      // Create our map, giving it the streetmap and earthquakes layers to display on load
+  var myMap = L.map("map", {
+    center: [
+      37.09, -95.71
+    ],
+    zoom: 5,
+    layers: [light]
+  });
+
+  counties = L.geoJSON(geoJson).addTo(myMap);
+
+  // Define a baseMaps object to hold our base layers
+  var baseMaps = {
+    "Light": light
+  };
+
+  // Create overlay object to hold our overlay layer
+  var overlayMaps = {
+    "Counties": counties
+  };
+
+
+
+  L.control.layers(baseMaps, overlayMaps).addTo(myMap);
+  // Create a layer control
+  // Pass in our baseMaps and overlayMaps
+  // Add the layer control to the map
