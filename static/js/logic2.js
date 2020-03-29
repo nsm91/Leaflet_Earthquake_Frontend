7c083ce4feabd7a2bbdddddb47a0582de6ea3e6e
Neil Mudjer <neil.mudjer@gmail.com>
2020-03-29 15:03:15 -0500

    removed config for 'pages'

--- static/js/logic2.js
+++ static/js/logic2.js
@@ -70,28 +70,28 @@ function createMap(earthquakes, tectonicPlates) {
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
     maxZoom: 18,
     id: "mapbox.light",
-    accessToken: API_KEY
+    accessToken: "pk.eyJ1IjoiZGFtdWRqZTkxIiwiYSI6ImNrM3RlZzBveTAyMWgzZW4yMmhqdm5iNm4ifQ.hq8K8gwIF5QKuQJFaMyaiA"
     });
 
     var outdoors = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
     maxZoom: 18,
     id: "mapbox.outdoors",
-    accessToken: API_KEY
+    accessToken: "pk.eyJ1IjoiZGFtdWRqZTkxIiwiYSI6ImNrM3RlZzBveTAyMWgzZW4yMmhqdm5iNm4ifQ.hq8K8gwIF5QKuQJFaMyaiA"
     });
 
     var streetmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
     maxZoom: 18,
     id: "mapbox.streets",
-    accessToken: API_KEY
+    accessToken: "pk.eyJ1IjoiZGFtdWRqZTkxIiwiYSI6ImNrM3RlZzBveTAyMWgzZW4yMmhqdm5iNm4ifQ.hq8K8gwIF5QKuQJFaMyaiA"
     });
 
     var dark = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
     maxZoom: 18,
     id: "mapbox.dark",
-    accessToken: API_KEY
+    accessToken: "pk.eyJ1IjoiZGFtdWRqZTkxIiwiYSI6ImNrM3RlZzBveTAyMWgzZW4yMmhqdm5iNm4ifQ.hq8K8gwIF5QKuQJFaMyaiA"
     });
 
   // Define a baseMaps object to hold our base layers
